93a9e6c8d1b0cb3e8e82b7878b1f7902d1fa45ab
sb255 <samarthbais1@gmail.com>
2019-07-11 20:05:08 +0530

    StackAndStringBuffer

---
+++ StackAndStringBuffer.java
@@ -0,0 +1,39 @@
+// String Question
+
+// Input: "TOY"
+
+// Output: return a String "Y-O" for the given input.
+
+import java.util.*;
+
+
+public class Test{
+
+    static String StringManipulation(String s){
+    
+        Stack<Character> st = new Stack<>(); 
+        
+        for(int i=0; i<s.length(); i++){
+            st.push(s.charAt(i));
+        }
+        
+        StringBuilder sb = new StringBuilder(); 
+        
+        sb.append(st.pop()); 
+        sb.append("-"); 
+        sb.append(st.pop()); 
+        
+        return sb.toString();
+    }
+    
+    public static void main(String[] args){
+        
+       String input = "TOY";
+       
+       System.out.print(StringManipulation(input));
+        
+    }
+        
+}
+
+
