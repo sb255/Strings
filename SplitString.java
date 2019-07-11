7a3335953f0497a4809e493270b513c5d49e83f6
sb255 <samarthbais1@gmail.com>
2019-07-11 13:39:31 +0530

    SplitString.java

---
+++ SplitString.java
@@ -0,0 +1,32 @@
+// use of split() in Strings
+
+import java.util.*;
+
+
+public class Test{
+        
+    public static void main(String[] args){
+        
+        String s = "Hello Welcome to the String World"; 
+        
+        Stack<String> ss = new Stack<>(); 
+        
+        for(String x : s.split(" ")){
+            
+            ss.push(x);
+            
+        }
+        
+        int size = ss.size(); 
+        
+        for(int i=0; i<size; i++){
+        
+            System.out.print(ss.pop()+" ");
+        
+        }
+        
+    }
+        
+}
+
+
