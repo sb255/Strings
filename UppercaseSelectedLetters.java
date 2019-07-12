2ba3dd53bad3937e1bd4f03b6c77a5450e759497
sb255 <samarthbais1@gmail.com>
2019-07-12 10:35:24 +0530

    Uppercasing selected letters

---
+++ UppercaseSelectedLetters.java
@@ -0,0 +1,28 @@
+// String Question
+
+// Uppercaseing the selected letters in a Stirng!
+
+// Input: helloworld
+
+// Output: HelloWorld
+
+import java.util.*;
+
+
+public class Test{
+
+    
+    public static void main(String[] args){
+
+        String input = "helloworld"; 
+        
+        String output = input.substring(0,1).toUpperCase() + input.substring(1,5) + 
+                        
+                        input.substring(5,6).toUpperCase() + input.substring(6); 
+        
+        System.out.println(output);
+    }
+        
+}
+
+
